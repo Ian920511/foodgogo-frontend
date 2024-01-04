5cf85b9bfb4b20546098b76400fd43fc49911352
An Lin <august810511@gmail.com>
2024-01-04 11:22:24 +0800

    feat: add favorite apis feature

--- src/apis/users.js
+++ src/apis/users.js
@@ -10,4 +10,24 @@ export default {
       throw error.response.data
     }
   },
+
+  async addFavorite(productId) {
+    try {
+      const response = await apiHelper.post(`/favorite/${productId}`)
+
+      return response.data
+    } catch (error) {
+      throw error.response.data
+    }
+  },
+
+  async removeFavorite(productId) {
+    try {
+      const response = await apiHelper.delete(`/favorite/${productId}`)
+
+      return response.data
+    } catch (error) {
+      throw error.response.data
+    }
+  }
 }
