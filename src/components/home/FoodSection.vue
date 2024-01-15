bdfb503bbbf994f891fb1c76a1e339be06b73f47
An Lin <august810511@gmail.com>
2024-01-15 20:10:14 +0800

    feat: adjust section jpg

--- src/components/home/FoodSection.vue
+++ src/components/home/FoodSection.vue
@@ -1,5 +1,35 @@
+<!-- <template>
+  <section class="position-relative">
+    <img src="./../../assets/section.jpg" alt="圖片" style="height: 40vh; object-fit: cover;">
+  </section>
+</template> -->
+
 <template>
-  <section class="position-relative"
-    style="height: 40vh; background-image: url('./../../../public/section.jpg'); background-repeat: no-repeat; background-position: center center; background-size: cover;">
+  <section class="position-relative">
+    <div class="image-container">
+      <img src="./../../assets/section.jpg" alt="圖片">
+    </div>
   </section>
 </template>
+
+<style>
+.position-relative {
+  position: relative;
+  height: 40vh;
+  overflow: hidden;
+}
+
+.image-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+
+img {
+  height: 100%;
+  width: auto;
+  object-fit: cover;
+}</style>
