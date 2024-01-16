3954010c0357ad7ae52a8cdbe90b4f20cd413a54
An Lin <august810511@gmail.com>
2024-01-16 13:40:42 +0800

    fix: adjust section jpg

--- src/components/home/FoodSection.vue
+++ src/components/home/FoodSection.vue
@@ -1,14 +1,6 @@
-<!-- <template>
-  <section class="position-relative">
-    <img src="./../../assets/section.jpg" alt="圖片" style="height: 40vh; object-fit: cover;">
-  </section>
-</template> -->
-
 <template>
   <section class="position-relative">
-    <div class="image-container">
-      <img src="./../../assets/section.jpg" alt="圖片">
-    </div>
+    <div class="background-image"></div>
   </section>
 </template>
 
@@ -16,20 +8,13 @@
 .position-relative {
   position: relative;
   height: 40vh;
-  overflow: hidden;
 }
 
-.image-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.background-image {
   height: 100%;
-  width: 100%;
-  overflow: hidden;
+  background-image: url('/src/assets/section.jpg');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
 }
-
-img {
-  height: 100%;
-  width: auto;
-  object-fit: cover;
-}</style>
+</style>
